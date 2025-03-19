87c96d3c6680988fe9e890900d3633140f323927
Christoffer Rozenbachs <christoffer.rozenbachs@gmail.com>
2025-03-19 09:54:22 +0100

    Commented out the map_1 function.

--- include/map.h
+++ include/map.h
@@ -3,12 +3,12 @@
 #include "tiles.h"
 
 void map_1(){
-    tiles[5][5].type = SAND;
-    tiles[5][6].type = SAND;
-    tiles[5][7].type = SAND;
-    tiles[5][8].type = SAND;
-    tiles[5][9].type = SAND;
-    tiles[5][9].type = SAND;
+    // tiles[5][5].type = SAND;
+    // tiles[5][6].type = SAND;
+    // tiles[5][7].type = SAND;
+    // tiles[5][8].type = SAND;
+    // tiles[5][9].type = SAND;
+    // tiles[5][9].type = SAND;
 }
 
 #endif // map.h
